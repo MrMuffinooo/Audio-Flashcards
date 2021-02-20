c4cc1ba5ddeb892c50fa90336ee982931222dc29
Karol Niewiadowski <speakpolishpl@gmail.com>
2021-02-20 15:10:25 +0100

    added activity navigation, deleted excessive bars

--- app/src/main/java/com/muffin/audioflashcards/MainActivity.kt
+++ app/src/main/java/com/muffin/audioflashcards/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.muffin.audioflashcards
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +20,16 @@ class MainActivity : AppCompatActivity() {
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+
+        findViewById<Button>(R.id.btn_play).setOnClickListener {
+            val intent = Intent(this, PlayActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.btn_learn).setOnClickListener {
+            val intent = Intent(this, LearnActivity::class.java)
+            startActivity(intent)
         }
 
     }
---
+++ app/src/main/java/com/muffin/audioflashcards/PlayActivity.kt
@@ -0,0 +1,21 @@
+package com.muffin.audioflashcards
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Spinner
+
+class PlayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+
+
+    }
+}
