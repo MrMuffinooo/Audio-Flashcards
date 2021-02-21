c87882b70a55e86b99b0e238a01c543a58f7212e
Karol Niewiadowski <speakpolishpl@gmail.com>
2021-02-21 18:09:05 +0100

    worked on flashcard classes

--- app/src/main/java/com/muffin/audioflashcards/FlashCard.kt
+++ app/src/main/java/com/muffin/audioflashcards/FlashCard.kt
@@ -1,15 +1,15 @@
 package com.muffin.audioflashcards
 
+import java.util.*
+
 class FlashCard(var word: String, var translation: String) {
     companion object {
         var id: Int = 1;
     }
 
-    var fails: Int = 0
-    var successes: Int = 0
+    var answerBalance:Int = 0
     var learned: Boolean = false // if user wants to listen to other flashcards
     val ID = id
+    var lastListen: Date = Date()
 
-
-    //TODO(implement class)
 }
--- app/src/main/java/com/muffin/audioflashcards/FlashcardsStorage.kt
+++ app/src/main/java/com/muffin/audioflashcards/FlashcardsStorage.kt
@@ -1,4 +1,58 @@
 package com.muffin.audioflashcards
 
+import java.util.ArrayList
+
 class FlashcardsStorage {
+    var list:ArrayList<FlashCard> = ArrayList()
+
+    fun addFlashcard(f:FlashCard){
+        if (f in list) return
+        list.add(f)
+    }
+
+    fun removeFlashcard(f:FlashCard){
+        list.remove(f)
+    }
+
+    fun getSize():Int{
+        return list.size
+    }
+
+    fun getSetToListen():FlashcardsStorage{
+        var r = FlashcardsStorage()
+        list.shuffle()
+
+       /* list.sortBy { it.answerBalance }
+        for (f in list){
+            r.addFlashcard(f)
+            if (r.getSize()>=5) break
+        }
+        list.sortBy { it.lastListen }
+        for (f in list){
+            r.addFlashcard(f)
+            if (r.getSize()>=10) break
+        }*/
+        for (f in list){
+            r.addFlashcard(f)
+            if (r.getSize()>=10) break
+        }
+
+
+        return r
+    }
+
+    fun getSetToPlay():FlashcardsStorage{
+        var r = FlashcardsStorage()
+        list.shuffle()
+
+        for (f in list){
+            r.addFlashcard(f)
+            if (r.getSize()>=10) break
+        }
+
+        return r
+
+    }
 }
+
+
