de77dbb7216e9ee20e2341fed6f38bfe5f2cbcdb
Karol Niewiadowski <speakpolishpl@gmail.com>
2021-03-11 17:44:48 +0100

    added comunication service -> activity via broadcast intent

--- app/src/main/java/com/muffin/audioflashcards/LearnActivity.kt
+++ app/src/main/java/com/muffin/audioflashcards/LearnActivity.kt
@@ -1,21 +1,25 @@
 package com.muffin.audioflashcards
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
+import android.content.*
 import android.os.Bundle
 import android.os.IBinder
 import android.view.View
-import android.widget.Button
+import android.view.animation.AlphaAnimation
 import android.widget.ImageButton
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+
 
 class LearnActivity : AppCompatActivity() {
 
+    lateinit var progress: ProgressBar
     lateinit var mService: SpeechService
     var isBound = false
+    lateinit var receiver: BroadcastReceiver
+    lateinit var txt_word: TextView
+    lateinit var txt_trans: TextView
 
     val serviceConnection = object: ServiceConnection{
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
@@ -34,6 +38,9 @@ class LearnActivity : AppCompatActivity() {
         setContentView(R.layout.activity_learn)
 
 
+        txt_word = findViewById(R.id.txt_to_translate_audio)
+        txt_trans = findViewById(R.id.txt_translated_audio)
+        progress = findViewById(R.id.progressBar)
 
         val play = findViewById<ImageButton>(R.id.btn_start)
         val stop = findViewById<ImageButton>(R.id.btn_pause)
@@ -43,11 +50,19 @@ class LearnActivity : AppCompatActivity() {
         stop.visibility = View.VISIBLE
         stop.isEnabled = true
 
+        var fadeIn = AlphaAnimation(0.5f,1f)
+        fadeIn.duration = 500
+
+        var fadeOut = AlphaAnimation(1f,0.5f)
+        fadeOut.duration = 500
+
+
+
 
 
         val intent = Intent(this, SpeechService::class.java)
         startService(intent)    //maybe not needed ????
-        bindService(intent,serviceConnection,Context.BIND_AUTO_CREATE)
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
 
         play.setOnClickListener {
             play.visibility = View.INVISIBLE
@@ -70,6 +85,51 @@ class LearnActivity : AppCompatActivity() {
         findViewById<ImageButton>(R.id.btn_prev).setOnClickListener{
             mService.skipPrev()
         }
+
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val s = intent.getStringExtra(SpeechService.UI_broadcast)
+                when(s){
+                    "FLASHCARD" -> nextFlashcard(
+                        intent.getStringExtra("WORD")!!,
+                        intent.getStringExtra("TRANSLATION")!!,
+                        intent.getStringExtra("EXTRA")!!
+                    )
+
+                    "TRANSLATION" -> {
+                        txt_word.startAnimation(fadeOut)
+                        txt_trans.startAnimation(fadeIn)
+                    }
+
+                    "EXTRA" -> {
+
+                    }
+                }
+            }
+        }
+        mService.resumeReading()
+    }
+
+    fun nextFlashcard(W: String, T: String, E: String) {
+        txt_word.text = W
+        txt_trans.text = T
+        //TODO("extra")
+        progress.progress++
+        if (progress.progress > progress.max)
+            progress.progress = 0
+    }
+
+    override fun onStart() {
+        super.onStart()
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+            receiver,
+            IntentFilter(SpeechService.UI_broadcast)
+        )
+    }
+
+    override fun onStop() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
+        super.onStop()
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/muffin/audioflashcards/SpeechService.kt
+++ app/src/main/java/com/muffin/audioflashcards/SpeechService.kt
@@ -9,38 +9,48 @@ import android.content.Intent
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
+import android.speech.tts.TextToSpeech
 import androidx.core.app.NotificationCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.gson.Gson
+import java.util.*
 
 class SpeechService: Service() {
     private val binder = LocalBinder()
     var progress = 0
     var maxValue = 10
     var isPaused = false
+    lateinit var TTS_JP: TextToSpeech
+    val broadcaster = LocalBroadcastManager.getInstance(this);
+    companion object {
+        const val UI_broadcast = "UI_Update"
+    }
+
 
     lateinit var set: FlashcardsStorage
 
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         createNotifficationChannel()
-        val notifIntent = Intent(this, LearnActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(this, 0, notifIntent, 0)
+        makeNotification()
+        setParamFromPreferences()
 
-        val notif = NotificationCompat.Builder(this, "SpeechChannelId")
-                .setContentTitle("Audio Flashcards")
-                .setSmallIcon(R.drawable.ic_baseline_speaker_24)
-                .setContentIntent(pendingIntent)    //should go back to activity but makes new one instead
-                .build()
-        startForeground(1,notif)
+        TTS_JP = TextToSpeech(this, object : TextToSpeech.OnInitListener {
+            override fun onInit(status: Int) {
+                if (status == TextToSpeech.SUCCESS) {
+                    var result = TTS_JP.setLanguage(Locale.JAPANESE)
 
-        val prefs = getSharedPreferences("flashcards", Context.MODE_PRIVATE)
-        val prefsEditor = prefs.edit()
-        val gson = Gson()
-        val json: String? = prefs.getString("list", "")
-        val list: FlashcardsStorage = gson.fromJson(json, FlashcardsStorage::class.java)
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        TODO("install lang")
+                    }
+                } else {
+                    stopSelf()
+                }
 
-        set = list.getSetToListen()
+            }
+        })
 
-        maxValue = set.getSize()
 
         return START_NOT_STICKY
     }
@@ -53,6 +63,31 @@ class SpeechService: Service() {
             manager.createNotificationChannel(channel)
         }
     }
+
+    private fun makeNotification() {
+        val notifIntent = Intent(this, LearnActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notifIntent, 0)
+
+        val notif = NotificationCompat.Builder(this, "SpeechChannelId")
+                .setContentTitle("Audio Flashcards")
+                .setSmallIcon(R.drawable.ic_baseline_speaker_24)
+                .setContentIntent(pendingIntent)    //should go back to activity but makes new one instead
+                .build()
+        startForeground(1, notif)
+    }
+
+    fun setParamFromPreferences(){
+        val prefs = getSharedPreferences("flashcards", Context.MODE_PRIVATE)
+        val gson = Gson()
+        val json: String? = prefs.getString("list", "")
+        val list = gson.fromJson(json, FlashcardsStorage::class.java)
+
+        set = list.getSetToListen()
+
+        maxValue = set.getSize()
+    }
+
+
 
     override fun onBind(intent: Intent?): IBinder? {
         return binder
@@ -69,8 +104,10 @@ class SpeechService: Service() {
         while (!isPaused){
             var f = set.get(progress)
 
-            TODO("play flashcards")
-
+            //TODO("play flashcards")
+            sendResult("FLASHCARD", f.word, f.translation, f.extra)
+            sendResult("TRANSLATION")
+            //sendResult("EXTRA")
             progress++
             if (progress > maxValue)
                 progress = 0
@@ -83,17 +120,31 @@ class SpeechService: Service() {
     }
 
     fun skipNext(){
-        TODO("halt reading")
+        //TODO("halt reading")
         if (progress == maxValue)
             progress = 0
         else
             progress++
     }
     fun skipPrev(){
-        TODO("halt reading")
+       // TODO("halt reading")
         if (progress == 0)
             progress = maxValue
         else
             progress--
     }
+
+    fun sendResult(message: String, word: String ="", trans: String ="", extra: String ="") {
+        val intent = Intent(UI_broadcast)
+        intent.putExtra(UI_broadcast, message)
+
+            intent.putExtra("WORD",word)
+            intent.putExtra("TRANSLATION",trans)
+            intent.putExtra("EXTRA",extra)
+
+
+        broadcaster.sendBroadcast(intent)
+    }
+
+
 }
